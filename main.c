8036a1c4f315163cd271a3f1beefc16d60c89701
luca48722 <155913731+luca48722@users.noreply.github.com>
2025-10-18 21:57:44 -0300

    Add files via upload
    Código

---
+++ main.c
@@ -0,0 +1,13 @@
+/* 
+ Projeto: REGISTER
+ Autor: Lucas Gabriel e Gusthavo H. S. Rodrigues
+ Data de criação: 14/10/2025
+ Descrição: Estrutura inicial do projeto integrador.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Iniciando o Projeto Integrador...\n");
+    return 0;
+}
